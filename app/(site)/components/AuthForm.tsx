da1af5619486defa94848bf8c28db0844ea7637e
TreNgheDiCode <gabayan170@gmail.com>
2023-06-03 11:37:49 +0700

    Sidebar, Navigation and Layout

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,7 +5,7 @@ import { BsLinkedin, BsDiscord, BsFacebook, BsGoogle, BsGithub } from 'react-ico
 
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { 
     SubmitHandler, 
     FieldValues, 
@@ -14,13 +14,22 @@ import {
 import AuthSocialButton from "./AuthSocialButton";
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (session?.status == 'authenticated') {
+            router.push('/users')
+        }
+    }, [session?.status, router])
 
     const toggleVariant = useCallback(() => {
         if (variant == 'LOGIN') {
@@ -47,7 +56,10 @@ const AuthForm = () => {
 
         if (variant == 'REGISTER') {
             axios.post('/api/register', data)
-            .then(() => toast.success('Đăng ký thành công'))
+            .then(() => {
+                toast.success('Đăng ký thành công');
+                signIn('credentials', data);
+            })
             .catch(() => toast.error('Vui lòng nhập đầy đủ thông tin'))
             .finally(() => setIsLoading(false));
         }
@@ -64,6 +76,7 @@ const AuthForm = () => {
 
                 if (callback?.ok && !callback?.error) {
                     toast.success("Đăng nhập thành công");
+                    router.push('/users')
                 }
             })
             .finally(() => setIsLoading(false));
